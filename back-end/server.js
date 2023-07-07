8f9d7ba2566d8ba4d6ed664d6768116b463169d9
sbcodes90 <sydneycburrell29@gmail.com>
2023-07-06 21:54:55 -0400

    add cors, display data from database yay!

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes/routes');
+const cors = require("cors");
 const mongoInfo = process.env.DB_KEY;
 
 
@@ -23,6 +24,14 @@ app.use(express.json());
 
 app.use('/api', routes)
 
-app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+const corsOptions ={
+    origin:'http://localhost:8080', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
+app.use(cors(corsOptions));
+
+app.listen(8080, () => {
+    console.log(`Server Started at ${8080}`)
 })
--- front-end/my-app/src/Home.js
+++ front-end/my-app/src/Home.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function Home() {
+const [data, setData] = useState([])
+
+    const url = `http://localhost:8080/api/getAll`;
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    const getData = async () => {
+        const result = await axios.get(
+            'http://localhost:8080/api/getAll',
+        {
+        
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+        console.log('result', result.data)
+        setData(result.data);
+        return result.data
+    }
+
+    useEffect(() => {
+      getData()
+    
+      return () => {
+        
+      }
+    }, [])
+    
     return (
         <div className="content-container">
             <h1>Home</h1>
+            {data.map((item)=> {
+               return <div key={item.id}>
+                <div>UserName:{item.name}</div>
+                <div>Email:{item.email}</div>
+                </div>
+            })}
         </div>
     )
 }
